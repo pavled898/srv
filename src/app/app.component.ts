78c1f962adc8ffd7882f0177f8a2da40c867452d
Pavle Delic <pavle.delic@everseen.com>
2020-03-06 15:57:13 +0100

    maps in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'Servis Računara';
+
+  isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  ngOnInit() {
+    const mobileCta: HTMLElement = document.querySelector(".btn--mobile-cta");
+    console.log(mobileCta)
+    mobileCta.style.display = this.isMobile ? "inline-block" : "none";
+  }
 }
