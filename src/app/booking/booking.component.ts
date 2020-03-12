93e6186be6f14a9b5a30f7fdbbd789bfa0d2fb24
Pavle Delic <pavle.delic@everseen.com>
2020-03-12 16:53:54 +0100

    .

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -6,8 +6,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent implements OnInit {
+  private months: Array<string> = [
+    "placeholder",
+    'januar',
+    'februar',
+    'mart',
+    'april',
+    'maj',
+    'jun',
+    'jul',
+    'avgust',
+    'septembar',
+    'oktobar',
+    'novembar',
+    'decembar'
+  ]
 
-  constructor() { }
+  
+  private viewDays: Array<number>;
+  private viewYear: number;
+  private viewMonth;
+  private selectedDate: Date;
+  private selectedDay: number;
+  private selectedMonth: number;
+  private selectedYear: number;
+
+  constructor() {
+    this.selectDate(new Date())
+    this.initView(this.selectedMonth, this.selectedYear)
+   }
+
+   initView(month, year) {
+    this.viewMonth = month;
+    this.viewYear = year;
+    this.daysInMonth(this.viewMonth,this.viewYear);
+
+   }
+
+   selectDate(date: Date) {
+    this.selectedDate = date;
+    this.selectedDay = this.selectedDate.getDate();
+    this.selectedMonth =  this.selectedDate.getMonth() + 1;
+    this.selectedYear = this.selectedDate.getFullYear();
+
+    console.log(this.months[this.selectedMonth]);
+   }
+   
+   daysInMonth (month, year) { 
+    const numberOfDays = new Date(year, month, 0).getDate(); 
+    this.viewDays = Array(numberOfDays).fill(0).map((x,i)=>i+1)
+    debugger
+ } 
+
+  firstWeekDay(month, year) {
+    return new Date(year, month, 1).getDay();
+  }
 
   ngOnInit() {
   }
